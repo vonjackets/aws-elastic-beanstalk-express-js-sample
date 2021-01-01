604b6676c967e1cf5c2d52ab256013530834d7f7
Von <vcaaron@rocketemail.com>
2021-01-01 14:44:39 -0500

    communism won

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('The World is mine.'));
+app.get('/', (req, res) => res.send('The world is ours.')
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
